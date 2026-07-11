e1cbdcdf47ebe650c6723ab2c75b0447fdacad9d
Fabioramos12br <binho.ramos@hotmail.com.br>
2026-07-11 12:48:38 -0300

    feat: centraliza e finaliza as rotas logicas de login, cadastro e sessao

--- src/app.py
+++ src/app.py
@@ -5,7 +5,40 @@ app = Flask(__name__)
 app.secret_key = 'chave_secreta_para_desafio_logistica'
 
 @app.route('/')
+
 def home():
     if 'usuario' not in session:
         return redirect(url_for('login'))
     return render_template('home.html', usuario=session['usuario'])
+
+@app.route('/login', methods=['GET', 'POST'])
+
+def login():
+    erro = None
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        conexao = obter_conexao()
+        cursor = conexao.cursor(dictionary=True)
+        
+        query = "SELECT * FROM tbl_login WHERE email = %s AND senha = MD5(%s)"
+        cursor.execute(query, (email, password))
+        user = cursor.fetchone()
+        
+        cursor.close()
+        conexao.close()
+        
+        if user:
+            session['usuario'] = user['nome']  
+            return redirect(url_for('home'))
+            
+        erro = "E-mail ou senha incorretos!"
+            
+    return render_template('login.html', erro=erro)
+
+@app.route('/logout')
+
+def logout():
+    session.pop('usuario', None)
+    return redirect(url_for('login'))
